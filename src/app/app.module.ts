7493f85723df8655e5274bd2fd660e2252f4f690
Xuyang Han <hanxuyang0826@gmail.com>
2021-04-28 20:05:54 -0400

    1. add ArcGIS package 2. add an empty side bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
