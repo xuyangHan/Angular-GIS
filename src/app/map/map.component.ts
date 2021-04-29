c116fa3d6f90ce57d42f0d081205ba2b226da1ce
Xuyang Han <hanxuyang0826@gmail.com>
2021-04-29 14:00:17 -0400

    change ts format to ;atest version

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { loadModules } from 'esri-loader';
+
+import EsriMap from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
 
 @Component({
   selector: 'app-map',
@@ -9,31 +11,19 @@ import { loadModules } from 'esri-loader';
 
 export class MapComponent implements OnInit {
 
-  // this is needed to be able to create the MapView at the DOM element in this component
-  @ViewChild('mapViewNode') private mapViewEl: ElementRef;
-
   constructor() { }
-
+  
   ngOnInit() {
-    loadModules([
-      'esri/Map',
-      'esri/views/MapView'
-    ])
-      .then(([EsriMap, EsriMapView]) => {
-        const map = new EsriMap({
-          basemap: 'streets'
-        });
+    const map = new EsriMap({
+      basemap: "streets-vector"
+    });
 
-        const mapView = new EsriMapView({
-          container: this.mapViewEl.nativeElement,
-          center: [-79.502938, 43.767854],
-          zoom: 15,
-          map: map
-        });
-      })
-      .catch(err => {
-        console.error(err);
-      });
+    const view = new MapView({
+      map: map,
+      container: "viewDiv",
+      center: [-79.502938, 43.767854],
+      zoom: 12
+    });
   }
-
 }
+
