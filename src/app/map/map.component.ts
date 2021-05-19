b2ed0ed31f329de9e55528f24679060f45cea0ff
Xuyang Han <hanxuyang0826@gmail.com>
2021-05-19 16:34:32 -0400

    change mode

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -118,6 +118,7 @@ export class MapComponent {
     // draw trajectory in track mode
     let totalDistance = 0;
     let totalCal = 0;
+    let speed_list = [0];
     track.on("track", function (trackEvent) {
 
       // console.log(trackEvent);
@@ -140,52 +141,6 @@ export class MapComponent {
 
       drawPointdrawLine(pos, point, graphicsLayer);
 
-      totalDistance += distanceInKmBetweenEarthCoordinates(pos[pos.length - 2][0], pos[pos.length - 2][1], current_location[0], current_location[1]);
-      MapComponent.prototype.totalDistance = Math.round(totalDistance);
-    })
-
-
-    // use fake location, every 5 seconds
-    // create a layer to draw
-    const graphicsLayer = new GraphicsLayer();
-    map.add(graphicsLayer);
-    
-    let speed_list = [0];
-    let labels = ['1'];
-    // let canvas = <HTMLCanvasElement>document.getElementById('myChart');
-    // let ctx = canvas.getContext('2d');
-
-    // this.Speed = speed_list;
-    // var data = {
-    //   labels: labels,
-    //   datasets: [{
-    //     label: 'My First Dataset',
-    //     data: speed_list,
-    //     fill: false,
-    //     borderColor: 'rgb(75, 192, 192)',
-    //     tension: 0.1
-    //   }]
-    // };
-    // var myChart = new Chart(ctx, {
-    //   type: 'line',
-    //   data: data,      
-    // });
-
-    let timer;
-    timer = setInterval(function () {
-      let random_lat_diff = (Math.random() - 0.25) * 0.001;
-      let random_long_diff = (Math.random() - 0.25) * 0.001;
-      let current_location = [pos[pos.length - 1][0] + random_lat_diff, pos[pos.length - 1][1] + random_long_diff]
-      pos.push(current_location);
-
-      // draw current point
-      const point = { //Create a point
-        type: "point",
-        longitude: current_location[0],
-        latitude: current_location[1]
-      };
-
-      drawPointdrawLine(pos, point, graphicsLayer);
       let newDist = distanceInKmBetweenEarthCoordinates(pos[pos.length - 2][0], pos[pos.length - 2][1], current_location[0], current_location[1]);
       totalDistance += newDist;
       MapComponent.prototype.totalDistance = Math.round(totalDistance);
@@ -198,34 +153,43 @@ export class MapComponent {
       let BMR = 1794;
       totalCal += METs * 5 / 3600 * BMR / 24;
       MapComponent.prototype.totalCal = Math.round(totalCal);
+    })
 
-      let dateTime = new Date()
-      let currtTime = dateTime.getHours() + '-' + dateTime.getMinutes() + '-' + dateTime.getSeconds();
-      labels.push(currtTime);
 
-      // var data = {
-      //   labels: labels,
-      //   datasets: [{
-      //     label: 'My First Dataset',
-      //     data: speed_list,
-      //     fill: false,
-      //     borderColor: 'rgb(75, 192, 192)',
-      //     tension: 0.1
-      //   }]
-      // };
-      // var myChart = new Chart(ctx, {
-      //   type: 'line',
-      //   data: data,
-      //   options: {
-      //     scales: {
-      //       y: {
-      //         beginAtZero: true
-      //       }
-      //     }
-      //   }
-      // });
+    // // use fake location, every 5 seconds
+    // // create a layer to draw
+    // const graphicsLayer = new GraphicsLayer();
+    // map.add(graphicsLayer);
 
-    }, 5000);
+    // let timer;
+    // timer = setInterval(function () {
+    //   let random_lat_diff = (Math.random() - 0.25) * 0.001;
+    //   let random_long_diff = (Math.random() - 0.25) * 0.001;
+    //   let current_location = [pos[pos.length - 1][0] + random_lat_diff, pos[pos.length - 1][1] + random_long_diff]
+    //   pos.push(current_location);
+
+    //   // draw current point
+    //   const point = { //Create a point
+    //     type: "point",
+    //     longitude: current_location[0],
+    //     latitude: current_location[1]
+    //   };
+
+    //   drawPointdrawLine(pos, point, graphicsLayer);
+    //   let newDist = distanceInKmBetweenEarthCoordinates(pos[pos.length - 2][0], pos[pos.length - 2][1], current_location[0], current_location[1]);
+    //   totalDistance += newDist;
+    //   MapComponent.prototype.totalDistance = Math.round(totalDistance);
+
+    //   let currSpeed = Math.round(newDist / 5 * 3600 / 1000);
+    //   speed_list.push(currSpeed);
+    //   MapComponent.prototype.Speed = speed_list;
+
+    //   let METs = 1.02532235513438 * currSpeed - 0.100512661177567;
+    //   let BMR = 1794;
+    //   totalCal += METs * 5 / 3600 * BMR / 24;
+    //   MapComponent.prototype.totalCal = Math.round(totalCal);
+
+    // }, 5000);
 
   }
 }
